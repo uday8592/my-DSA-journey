73c4a23e4929d6f944f384cb406c264fb337bac5
CHINTHAKINDI UDAYKIRAN <135016621+uday8592@users.noreply.github.com>
2024-12-15 20:41:34 +0530

    Time: 1 ms (99.87%), Space: 46.1 MB (55.64%) - LeetHub

--- 0136-single-number/0136-single-number.java
+++ 0136-single-number/0136-single-number.java
@@ -1,7 +1,8 @@
 class Solution {
     public int singleNumber(int[] nums) {
     int result=0;
-    for(int num:nums){
+    for(int num:nums){    // exor with same numgives 0 
+                          // exor with dif num give the same num
         result=result^num;
     }
     return result;
